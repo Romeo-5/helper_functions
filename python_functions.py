cfb560d11ca5fa21bf63ed85a1f8a55d694ddddc
Romeo Nickel <106633433+Romeo-5@users.noreply.github.com>
2023-07-19 17:59:13 -0700

    Create python_functions.py

---
+++ python_functions.py
@@ -0,0 +1,27 @@
+# Plot the validation and training curves separately 
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics
+  """
+  loss = history.history["loss"]
+  val_loss = history.history["val_loss"]
+
+  accuracy = history.history["accuracy"]
+  val_accuracy = history.history["val_accuracy"]
+
+  epochs = range(len(history.history["loss"])) # how many epochs did we run for?
+
+  # Plot loss 
+  plt.plot(epochs, loss, label="training_loss")
+  plt.plot(epochs, val_loss, label="val_loss")
+  plt.title("loss")
+  plt.xlabel("epochs")
+  plt.legend()
+
+  # Plot accuracy 
+  plt.plot(epochs, accuracy, label="training_accuracy")
+  plt.plot(epochs, val_accuracy, label="val_accuracy")
+  plt.title("accuracy")
+  plt.xlabel("epochs")
+  plt.legend()
+
